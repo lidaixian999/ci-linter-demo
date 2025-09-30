57ac8bbb8b9a8ed532ca5be3aa111704416769d0
kongshan <970494264@qq.com>
2025-09-30 18:32:35 +0800

    finish

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-import os
-import sys
-
-
 def hello_world():
     x = 1 + 2
-    print("Hello, world!")
-    very_long_variable_name_that_exceeds_the_pep8_line_length_limit_by_far = (
+    long_message = (
         "This line is definitely longer than 79 or even 88 characters, "
         "which violates PEP8."
     )
+    print("Hello, world!")
+    print(long_message)  # 使用它！
     return x
