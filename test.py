e5a4d53b6c81951281fb6c8951f777ec9afcaf28
kongshan <970494264@qq.com>
2025-09-30 18:43:51 +0800

    finish test.py

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-def hello_world():
-    x = 1 + 2
-    long_message = (
-        "This line is definitely longer than 79 or even 88 characters, "
-        "which violates PEP8."
-    )
-    print("Hello, world!")
-    print(long_message)  # 使用它！
-    return x
+def add(a, b):
+    return a + b
+print(add(1, 2))
